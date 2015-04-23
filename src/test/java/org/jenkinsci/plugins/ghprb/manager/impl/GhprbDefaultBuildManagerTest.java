80c8a8f15cb5647b8ee06fb771975389e35b1e53
Alexander Köplinger <alex.koeplinger@outlook.com>
2015-04-23 16:43:51 +0200

    Fix missing value for additional constructor argument added in 2aac65f2afda545c2db5ff544d770dae4a38aa5e

--- src/test/java/org/jenkinsci/plugins/ghprb/manager/impl/GhprbDefaultBuildManagerTest.java
+++ src/test/java/org/jenkinsci/plugins/ghprb/manager/impl/GhprbDefaultBuildManagerTest.java
@@ -65,7 +65,7 @@ public class GhprbDefaultBuildManagerTest extends GhprbITBaseTestCase {
 		MatrixProject project = jenkinsRule.createMatrixProject("MTXPRJ");
 
 		GhprbTrigger trigger = new GhprbTrigger("user", "user", "",
-			"*/1 * * * *", "retest this please", false, false, false, false,
+			"*/1 * * * *", "retest this please", false, false, false, false, false,
 			false, null, null, false, null, null, null);
 
 		given(commitPointer.getSha()).willReturn("sha");
